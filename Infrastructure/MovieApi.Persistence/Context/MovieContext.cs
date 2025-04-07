2c4bdbf5ac99edaf403144a1f540d5b7f0c8c839
azraep <65713783+azraep@users.noreply.github.com>
2025-04-07 15:07:13 +0300

    Ders 5

---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= DESKTOP-11AMGS3\\SQLEXPRESS;initial Catalog=01_ApiMovieDb;integrated security=True;TrustServerCertificate=True");
+        }
+        #region Tabloları Hazırlama
+        // public DbSet < C# tablo adı > Sql Tablo Adı { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+
+        #endregion
+    }
+}
