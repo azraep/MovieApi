f615ad931436f00b8512b94b513ed7fc3e218905
azraep <65713783+azraep@users.noreply.github.com>
2025-04-07 21:46:48 +0300

    Ders 8

---
+++ Core/MoviApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/UpdateMovieCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MoviApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class UpdateMovieCommandHandler
+    {
+        private readonly MovieContext _context;
+
+        public UpdateMovieCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async void Handle(UpdateMovieCommand command)
+        {
+            var value = await _context.Movies.FindAsync(command.MovieId);
+            value.Rating = command.Rating;
+            value.Duration = command.Duration;
+            value.Status = command.Status;
+            value.CoverImageUrl = command.CoverImageUrl;
+            value.CreatedYear = command.CreatedYear;
+            value.Description = command.Description;
+            value.ReleaseDate = command.ReleaseDate;
+            value.Title = command.Title;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
